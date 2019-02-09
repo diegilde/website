d0e732318898067d9d61a3359172aabe27c03c09
Die Gilde <team@die-gilde.online>
2019-02-09 16:52:57 +0100

    refactor deploy-github script

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -1,25 +1,42 @@
 const fs = require('fs')
+const { promisify } = require('util')
 const ghpages = require('gh-pages')
 
-fs.writeFile('public/CNAME', "jointheguild.online", function(err) {
-  console.error(err)
-});
+const writeFile = promisify(fs.writeFile);
+const publishToGithub = promisify(ghpages.publish);
 
-ghpages.publish(
-  'public',
-  {
-    user: {
-      name: 'Die Gilde',
-      email: 'team@die-gilde.online'
-    },
-    branch: 'master',
-    repo: 'git@github.com:diegilde/diegilde.github.io.git',
-  },
-  (err) => {
-    if (err) {
-      console.error(err)
-    } else {
-      console.log('Deploy Complete! 🎉')
+async function writeCNAMEIntoTranspiledFiles() {
+  console.log('Add CNAME file to compiled files...')
+  await  writeFile('public/CNAME', "jointheguild.online")
+  console.log('CNAME added successfully!')
+}
+
+async function publishToGithubPages() {
+  console.log('Start publishing...')
+  await publishToGithub(
+    'public',
+    {
+      user: {
+        name: 'Die Gilde',
+        email: 'team@die-gilde.online'
+      },
+      branch: 'master',
+      repo: 'git@github.com:diegilde/diegilde.github.io.git',
     }
-  }
-)
+  )
+  console.log('Website published!')
+}
+
+(async function() {
+  console.log('︎\n\n📣 Deployment to github pages started...︎📣\n')
+  await writeCNAMEIntoTranspiledFiles()
+  await publishToGithubPages()
+  console.log('\n✅ Deployment Completed!✅\n')
+})()
+
+
+
+
+
+
+
