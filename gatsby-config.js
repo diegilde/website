5721b92129718852abf7e75393a7793cc9607dc5
Die Gilde <team@die-gilde.online>
2019-02-09 19:39:33 +0100

    remove twitter card

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,5 @@ module.exports = {
         respectDNT: false,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
 }
