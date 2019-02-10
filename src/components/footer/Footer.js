72f1fde37d21f31873a33c4bd912f864cc25f092
Die Gilde <team@die-gilde.online>
2019-02-10 18:08:54 +0100

    add privacy policy

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,11 +6,13 @@ const Footer = () => (
   <footer className="footer">
     <div className="content">
       <div className="desktop">
-        © {new Date().getFullYear()}, The Guild | <Link to="/legal-notice">Legal Notice</Link> | <a href="mailto:team@jointheguild.online">Contact</a>
+        © {new Date().getFullYear()}, The Guild | <Link to="/legal-notice">Legal Notice</Link> | <Link to="/privacy-policy">Privacy Policy</Link> | <a href="mailto:team@jointheguild.online">Contact</a>
       </div>
       <div className="mobile">
-        © {new Date().getFullYear()}, The Guild<br />
-        <Link to="/legal-notice">Legal Notice</Link> | <a href="mailto:team@jointheguild.online">Contact</a>
+        <p>© {new Date().getFullYear()}, The Guild</p>
+        <p><Link to="/legal-notice">Legal Notice</Link></p>
+        <p><Link to="/privacy-policy">Privacy Policy</Link></p>
+        <p><a href="mailto:team@jointheguild.online">Contact</a></p>
       </div>
     </div>
   </footer>
