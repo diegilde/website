a388ae998b8b9aa003ec2bcb19804be4b972c42f
Die Gilde <team@die-gilde.online>
2019-02-10 16:46:08 +0100

    add desktop version for howitworks and organisations sections

--- src/components/howItWorks/HowItWorks.js
+++ src/components/howItWorks/HowItWorks.js
@@ -3,30 +3,47 @@ import Step from './Step';
 import "./HowItWorks.css"
 import bigTriangleDivider from '../../images/big-triangle-divider.svg';
 
+function Steps() {
+   return (
+     <>
+         <div>
+             <Step
+               number={1}
+               title="Personal mentoring & workshops"
+               description="Find your professional mentor or join a peer-hosted workshop to learn a new skill."
+             />
+             <Step
+               number={2}
+               title="Get valuable answers"
+               description="Struggling with a specific challenge or question? Ask your guild and receive valuable answers from other experts in your field."
+             />
+         </div>
+         <div>
+             <Step
+               number={3}
+               title="Curated relevant content"
+               description="Peer-curated and -rated content suggestions help you spend your time only on those blog posts and links that can really move you forward."
+             />
+             <Step
+               number={4}
+               title="Help others progress"
+               description="Engage in your guild and support others in their development by offering mentoring, content, workshops or answering questions."
+             />
+         </div>
+     </>
+   )
+}
+
 const HowItWorks = () => (
   <div className="howItWorks">
     <div className="content">
         <h1>How it works</h1>
-        <Step
-          number={1}
-          title="Personal mentoring & workshops"
-          description="Find your professional mentor or join a peer-hosted workshop to learn a new skill."
-        />
-        <Step
-          number={2}
-          title="Get valuable answers"
-          description="Struggling with a specific challenge or question? Ask your guild and receive valuable answers from other experts in your field."
-        />
-        <Step
-          number={3}
-          title="Curated relevant content"
-          description="Peer-curated and -rated content suggestions help you spend your time only on those blog posts and links that can really move you forward."
-        />
-        <Step
-          number={4}
-          title="Help others progress"
-          description="Engage in your guild and support others in their development by offering mentoring, content, workshops or answering questions."
-        />
+        <div className="desktop">
+          <Steps />
+        </div>
+        <div className="mobile">
+          <Steps />
+        </div>
     </div>
     <img src={bigTriangleDivider} className="divider" alt="divider" />
   </div>
