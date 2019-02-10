d2093834c488c4e7522ab0052c7e2c41d8cf499a
Die Gilde <team@die-gilde.online>
2019-02-10 16:54:28 +0100

    add desktop version for footer and about sections

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,13 +1,26 @@
 import React from "react"
 import "./About.css"
 
-const About = () => (
-  <div className="about">
-    <div className="content">
+function Content() {
+  return (
+    <>
       <h1>About</h1>
       <p>The Guild’s mission is to give agency to millions of professionals around the world by re-defining what professional development means for blue & white collar workers in the 21st century.</p>
       <p>The Guild is on an audacious mission. To achieve it, we require support and feedback from a wider audience of visionaries, technologists, economists, policymakers and other change makers who are capable of shaping the Future of Work with us.</p>
       <p>If you are interested in contributing to The Guild's mission, please get in touch.</p>
+    </>
+  )
+}
+
+const About = () => (
+  <div className="about">
+    <div className="content">
+      <div className="desktop">
+        <Content />
+      </div>
+      <div className="mobile">
+        <Content />
+      </div>
     </div>
   </div>
 )
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,8 +4,15 @@ import { Link } from 'gatsby';
 
 const Footer = () => (
   <footer className="footer">
-    © {new Date().getFullYear()}, The Guild<br />
-    <Link to="/legal-notice">Legal Notice</Link> | <a href="mailto:team@jointheguild.online">Contact</a>
+    <div className="content">
+      <div className="desktop">
+        © {new Date().getFullYear()}, The Guild | <Link to="/legal-notice">Legal Notice</Link> | <a href="mailto:team@jointheguild.online">Contact</a>
+      </div>
+      <div className="mobile">
+        © {new Date().getFullYear()}, The Guild<br />
+        <Link to="/legal-notice">Legal Notice</Link> | <a href="mailto:team@jointheguild.online">Contact</a>
+      </div>
+    </div>
   </footer>
 )
 
