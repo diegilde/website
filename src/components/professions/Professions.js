b8570367c0e1717ef8d70aebcf3da02ce04f34e8
Die Gilde <team@die-gilde.online>
2019-02-10 17:15:25 +0100

    add desktop version for professions section

--- src/components/professions/Professions.js
+++ src/components/professions/Professions.js
@@ -6,30 +6,47 @@ import nutritionExperts from '../../images/nutrition-experts.svg'
 import dataScientists from '../../images/data-scientists.svg'
 import "./Professions.css"
 
+function Profs() {
+  return (
+    <>
+      <div>
+        <div className="profession">
+          <img src={executiveAssistants} alt="Executive Assistants" />
+          <p>Executive Assistants</p>
+        </div>
+        <div className="profession">
+          <img src={socialSupportWorkers} alt="Social Support Workers" />
+          <p>Social Support Workers</p>
+        </div>
+        <div className="profession">
+          <img src={productManagers} alt="Product Managers" />
+          <p>Product Managers</p>
+        </div>
+      </div>
+      <div>
+        <div className="profession">
+          <img src={nutritionExperts} alt="Nutrition Experts" />
+          <p>Nutrition Experts</p>
+        </div>
+        <div className="profession">
+          <img src={dataScientists} alt="Data Scientists" />
+          <p>Data Scientists</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
 const Professions = () => (
   <div className="professions">
     <div className="content">
       <h1>Join your guild</h1>
       <p>The Guild is currently focusing on these five professions. If your profession is not one of them, let us know by selecting “Other” in the signup form. We will get in touch with you as soon as we are ready to launch.</p>
-      <div className="profession">
-        <img src={executiveAssistants} alt="Executive Assistants" />
-        <p>Executive Assistants</p>
+      <div className="desktop">
+        <Profs />
       </div>
-      <div className="profession">
-        <img src={socialSupportWorkers} alt="Social Support Workers" />
-        <p>Social Support Workers</p>
-      </div>
-      <div className="profession">
-        <img src={productManagers} alt="Product Managers" />
-        <p>Product Managers</p>
-      </div>
-      <div className="profession">
-        <img src={nutritionExperts} alt="Nutrition Experts" />
-        <p>Nutrition Experts</p>
-      </div>
-      <div className="profession">
-        <img src={dataScientists} alt="Data Scientists" />
-        <p>Data Scientists</p>
+      <div className="mobile">
+        <Profs />
       </div>
     </div>
   </div>
