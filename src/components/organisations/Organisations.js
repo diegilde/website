b5e901152f2d02c1e5c7d16d291eb8316abe7375
Die Gilde <team@die-gilde.online>
2019-02-10 19:33:31 +0100

    apply design adjustments

--- src/components/organisations/Organisations.js
+++ src/components/organisations/Organisations.js
@@ -7,7 +7,7 @@ const Organisations = () => (
     <div className="content">
         <h1>For organisations</h1>
         <div className="mobile">
-            <h4>Guilds provide a better, easier and cheaper way to develop talent.</h4>
+            <h3>Guilds provide a better, easier and cheaper way to develop talent.</h3>
             <Image />
             <p>Through a convenient monthly subscription, organisations can outsource their professional development programs into the guilds. THE GUILD shares this revenue through a unique ownership model with the members of the guilds (coming soon).</p>
             <p>Guilds allow organisations of all sizes to provide ’state of the art’ talent development that requires no internal resources, facilitate expert careers that can overcome ‘pyramid bottlenecks’, recruit faster and more sustainable.</p>
