1cdb6715afc7670fba4b703e8c71b020fffa0d04
Die Gilde <team@die-gilde.online>
2019-02-10 16:17:11 +0100

    add desktop version for the guild section

--- src/components/theGuild/TheGuild.js
+++ src/components/theGuild/TheGuild.js
@@ -5,6 +5,25 @@ import bigTriangleDivider from "../../images/big-triangle-divider.svg"
 import "./TheGuild.css"
 import BackgroundImage from './BackgroundImage'
 
+function Benefits() {
+    return (
+      <>
+          <Benefit
+            title="Free career development"
+            description="Get free support from your professional peers that will help you advance your career."
+          />
+          <Benefit
+            title="Reliable professional community"
+            description="Join your guild and find your 'professional home’ - reliable, trustworthy and independent from your employer. "
+          />
+          <Benefit
+            title="Rewarding engagement"
+            description="Benefit financially from the impact you have in the guild through a unique ownership model."
+         />
+      </>
+    )
+}
+
 const TheGuild = () => (
   <BackgroundImage className="theGuild">
     <img src={bigTriangleDivider} className="divider" alt="divider" />
@@ -12,18 +31,12 @@ const TheGuild = () => (
         <h1>The Guild supports your career development outside your organisation.</h1>
         <p>We incubate and host professional guilds on our platform. Each guild provides peer-2-peer career development for their members - independent from their employer, free and accessible to a wide range of professions.</p>
         <img className="illustration" src={theGuild} alt="What is The Guild illustration" />
-        <Benefit
-          title="Free career development"
-          description="Get free support from your professional peers that will help you advance your career."
-        />
-        <Benefit
-          title="Reliable professional community"
-          description="Join your guild and find your 'professional home’ - reliable, trustworthy and independent from your employer. "
-        />
-        <Benefit
-          title="Rewarding engagement"
-          description="Benefit financially from the impact you have in the guild through a unique ownership model."
-        />
+        <div className="desktop flex">
+          <Benefits />
+        </div>
+        <div className="mobile">
+          <Benefits />
+        </div>
     </div>
     <div className="bg" />
   </BackgroundImage>
