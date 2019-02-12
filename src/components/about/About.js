269cdb887ded1976465f0ef763d408f02542e1fd
te-geekette <tinaegolf@gmail.com>
2019-02-12 13:35:49 +0100

    Updated the style of the About section
    I wanted to make the entire section a bit smaller. I know the last sentence is not optimal where it stands right now but I still like it better. What do you think?

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,9 +5,14 @@ function Content() {
   return (
     <>
       <h1>About</h1>
-      <p>The Guild’s mission is to give agency to millions of professionals around the world by re-defining what professional development means for blue & white collar workers in the 21st century.</p>
-      <p>The Guild is on an audacious mission. To achieve it, we require support and feedback from a wider audience of visionaries, technologists, economists, policymakers and other change makers who are capable of shaping the Future of Work with us.</p>
-      <p>If you are interested in contributing to The Guild's mission, please get in touch.</p>
+      <div>
+        <p>The Guild’s mission is to give agency to millions of professionals around the world by re-defining what professional development means for blue & white collar workers in the 21st century.</p>
+        <p>To achieve this mission, we require support and feedback from a wider audience of visionaries, technologists, economists, policymakers and other change makers who are capable of shaping the Future of Work with us.</p>
+      </div>
+      <div>
+        <p></p>
+        <p id='contactus'> If you are interested in contributing to The Guild's mission, please get in touch.</p>
+      </div>
     </>
   )
 }
